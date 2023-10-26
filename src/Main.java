f16fb6df83946c105d6820e959f4dd8bf1e17095
TaeWooKim-SCH <zop1234@hanmail.net>
2023-10-26 16:48:54 +0900

    feat: 배열

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array1 = new int[100];
+
+        array1[0] = 50;
+        array1[50] = 100;
+
+        System.out.println(array1[0]);
     }
 }

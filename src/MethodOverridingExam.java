859ff6164d1578bddfc02a00b7c136fde3340826
TaeWooKim-SCH <zop1234@hanmail.net>
2023-10-27 15:27:22 +0900

    feat: MethodOverriding

---
+++ src/MethodOverridingExam.java
@@ -0,0 +1,6 @@
+public class MethodOverridingExam {
+    public static void main(String[] args) {
+        MethodOverriding m1 = new MethodOverriding();
+        System.out.println(m1.plus("hello", " world"));
+    }
+}
